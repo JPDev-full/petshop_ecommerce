27c0078cfd67531897450f567c20848ad43cd91c
João Protti Neto <120661541+JPDev-full@users.noreply.github.com>
2024-02-16 18:40:28 -0300

    Delete src directory

--- src/services/authService.ts
+++
@@ -1,7 +0,0 @@
-// src/services/authService.ts
-export function authenticateUser(username: string, password: string): boolean {
-    // Aqui você faz a autenticação do usuário
-    // Verifique se o username e o password estão corretos
-    // Se estiverem corretos, retorne true, senão, retorne false
-    return false;
-}
