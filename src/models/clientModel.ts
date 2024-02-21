ce07dbba5f9a370006e5144d53cfcbb64d800f70
Gabriel Albuquerque <gfonteles.a@gmail.com>
2024-02-21 18:57:17 -0300

    fix: ajustes de todos os arquivos para manter padrao de liguagem, assim como ajuste no schmema

--- src/models/clientModel.ts
+++ src/models/clientModel.ts
@@ -1,47 +1,44 @@
-import { PrismaClient } from '@prisma/client';
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 export interface Clients {
-  id_clients?: string;
+  clientId?: string;
   name: string;
   cpf: string; // Alterado para string
   email: string;
   phone: number;
-  password: string;
 }
 
 export async function createClient(clientData: Clients) {
-  const hashedPassword = await bcrypt.hash(clientData.password, 10);
-  const { id_clients, ...data } = clientData;
-  return await prisma.clients.create({ 
+  const { clientId, ...data } = clientData;
+  return await prisma.clients.create({
     data: {
       ...data,
-      id_clients: id_clients || uuidv4(),
-      password: hashedPassword,
-    }
+      id_client: clientId || uuidv4(),
+    },
   });
 }
 
 export async function getClienteById(id: string) {
-  return await prisma.clients.findUnique({ 
-    where: { id_clients: id }
+  return await prisma.clients.findUnique({
+    where: { id_client: id },
   });
 }
 
 export async function updateCliente(id: string, clientData: Clients) {
-  const { id_clients, ...data } = clientData;
-  return await prisma.clients.update({ 
-    where: { id_clients: id },
-    data: data
+  const { clientId, ...data } = clientData;
+  return await prisma.clients.update({
+    where: { id_client: id },
+    data: data,
   });
 }
 
 export async function deleteCliente(id: string) {
   return await prisma.clients.delete({
-    where: { id_clients: id }
+    where: { id_client: id },
   });
 }
 
