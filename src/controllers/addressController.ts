78db415e2dae26c6430e98df8c60c6b185bd9eec
Edson Suraty <edinhosuraty@gmail.com>
2024-02-22 06:20:20 -0300

    feat: implementa tipagem na validação

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -5,15 +5,15 @@ import * as AddressModel from '../models/addressModel';
 export async function createAddress(req: Request, res: Response) {
   try {
 
-    const { street, city, country, clientId } = req.body;
+    const addressData: AddressModel.IAddress = req.body;
 
-    if (!street || !city || !country || !clientId) {
+    if (!addressData.street || !addressData.city || !addressData.state || !addressData.cep) {
       return res.status(400).json({
         error: true,
         messege: 'Todos os campos são obrigatórios' });
     }
 
-    const address = await AddressModel.createAddress(req.body);
+    const address = await AddressModel.createAddress(addressData);
 
     return res.status(201).json({
         error: false,
@@ -21,6 +21,9 @@ export async function createAddress(req: Request, res: Response) {
         address
     });
   } catch (err: any){
-    return res.json({message: err.message})
+    return res.status(500).json({ 
+        error: true, 
+        message: 'Erro ao criar endereço' 
+    });
   }
 }
